45396ac83ecc9c7aeb02b67952cec72a67be6967
unknown <talissonmnunes@hotmail.com>
2021-10-24 20:18:17 -0300

    New: inclui testes para mouse hover

---
+++ spec/mouse_hover_spec.rb
@@ -0,0 +1,27 @@
+
+describe 'Mouse hover', :mouse_hover do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'quando passo o mouse sobre o blade' do
+        card = find('img[alt*=Blade]') #asterisco = contém determinado elemento na tela
+        card.hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end
+
+    it 'quando passo o mouse sobre o pantera negra' do
+        card = find('img[alt^=Pantera]') #acento circunflexo = contém elemento iniciado com "valor" na tela
+        card.hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end
+
+    it 'quando passo o mouse sobre o homem aranha' do
+        card = find('img[alt$=Aranha]') #dolar = contém elemento finalizando com "valor" na tela
+        card.hover
+
+        expect(page).to have_content 'Nome: Homem Aranha'
+    end
+end
