57c8530057240e034fbfdf988746ff773d138cad
unknown <talissonmnunes@hotmail.com>
2021-10-24 16:13:15 -0300

    Update: inclui novos testes para o login

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -15,4 +15,34 @@ describe 'Forms' do
             expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
         end
     end
+
+    it 'senha incorreta' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in "userId",	with: "stark" 
+        fill_in "password",	with: "teste!"
+        
+        click_button 'Login'
+
+        aggregate_failures 'testing login' do
+            expect(find('#flash').visible?).to be true
+            expect(find('#flash').text).to include 'Senha é invalida!'
+            expect(find('#flash')).to have_content 'Senha é invalida!'
+        end
+    end
+
+    it 'usuário não cadastrado' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in "userId",	with: "teste" 
+        fill_in "password",	with: "jarvis!"
+        
+        click_button 'Login'
+
+        aggregate_failures 'testing login' do
+            expect(find('#flash').visible?).to be true
+            expect(find('#flash').text).to include 'O usuário informado não está cadastrado!'
+            expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
+        end
+    end
 end
