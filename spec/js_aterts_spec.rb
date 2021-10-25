d3dfaf5974cb84a4833853297959ed6907e00754
unknown <talissonmnunes@hotmail.com>
2021-10-25 07:24:39 -0300

    New: cria testes para kyes e alerts

---
+++ spec/js_aterts_spec.rb
@@ -0,0 +1,49 @@
+
+describe 'Alertas de JS', :alerts do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/javascript_alerts'
+    end
+
+    it 'alerta' do
+        click_button 'Alerta'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+    end
+
+    it 'sim confirma' do
+        click_button 'Confirma'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'Mensagem confirmada'
+    end
+
+    it 'não confirma' do
+        click_button 'Confirma'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Mensagem não confirmada'
+    end
+
+    it 'accept prompt' do
+        accept_prompt(with: 'Talisson') do
+            click_button 'Prompt'
+        end
+
+        expect(page).to have_content 'Olá, Talisson'
+    end
+
+    it 'dismiss prompt' do
+        dismiss_prompt do
+            click_button 'Prompt'
+        end
+
+        expect(page).to have_content 'Olá, null'
+    end
+end
---
+++ spec/keys_pressing_spec.rb
@@ -0,0 +1,22 @@
+
+describe 'Pressionando teclado', :keys do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/key_presses'
+    end
+
+    it 'enviando teclas' do
+        teclas = %i[tab escape space enter shift control alt]
+        teclas.each do |tecla|
+            find('#campo-id').send_keys tecla
+            expect(page).to have_content 'You entered: ' + tecla.to_s.upcase
+        end
+    end
+
+    it 'enviando letras' do
+        letras = %w[a s d f g h j k l]
+        letras.each do |letra|
+            find('#campo-id').send_keys letra
+            expect(page).to have_content 'You entered: ' + letra.to_s.upcase
+        end
+    end
+end
